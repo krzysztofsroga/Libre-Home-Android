d40d9ea7b20db04eaf05c1e95659049d2649ac21
Krzysztof Sroga <krzysztof@sroga.pw>
2019-12-17 17:43:20 +0100

    added switchGroup entity to database

--- app/src/main/java/com/krzysztofsroga/librehome/database/SwitchesRoomDatabase.kt
+++ app/src/main/java/com/krzysztofsroga/librehome/database/SwitchesRoomDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [FavoriteSwitch::class], version = 1)
+@Database(entities = [FavoriteSwitch::class, SwitchGroup::class], version = 1)
 abstract class SwitchesRoomDatabase : RoomDatabase() {
     abstract val favoriteDao: FavoriteSwitchDao
     abstract val switchGroupDao: SwitchGroupDao
