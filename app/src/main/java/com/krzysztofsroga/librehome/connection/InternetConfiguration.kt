068c4f96d6c60690ec12fe334d141bf2ccf4271c
hkemal <hkemal@marvin>
2020-02-16 21:05:20 +0100

    AI-3.5.3 <hkemal@marvin Overwrite remote https://github.com/anonimno1/Libre-Home-Android.git to local

--- app/src/main/java/com/krzysztofsroga/librehome/connection/InternetConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package com.krzysztofsroga.librehome.connection
-
-object InternetConfiguration {
-    const val sshPort = 22
-    const val rPiUsername = "pi"
-    const val defaultDomoticzHostname = "domoticz"  //TODO read string resource "192.168.2.191"
-    const val defaultDomoticzPort = 8080
-}
--- app/src/main/java/com/krzysztofsroga/librehome/models/FavoriteSwitch.kt
+++
@@ -1,8 +0,0 @@
-package com.krzysztofsroga.librehome.models
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-
-@Entity(tableName = "favorites")
-data class FavoriteSwitch(@PrimaryKey val id: Int)
